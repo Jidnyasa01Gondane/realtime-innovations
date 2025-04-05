b76c55dc35df3c65fd6f1e0214e1c9f6ab90cb1e
Jidnyasa Gondane <jidnyasagondane@gmail.com>
2025-04-05 15:56:17 +0530

    Changes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.realtime.app',
+  appName: 'realtime-innovation-employee-app',
+  webDir: 'www'
+};
+
+export default config;
